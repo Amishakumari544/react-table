b812dedbaf231146c43a37b5113e99641397c9fe
Amisha Kumari <amishakumari544@gmail.com>
2021-09-15 22:03:02 +0530

    react-table

---
+++ src/components/columns.js
@@ -0,0 +1,31 @@
+export const COLUMNS = [
+  {
+    Header: 'Id',
+    accessor: 'id',
+    disableFilters: true,
+    sticky: 'left'
+  },
+  {
+    Header: 'Name',
+    accessor: 'name',
+    sticky: 'left'
+  },
+ 
+  {
+    Header: 'Age',
+    accessor: 'age',
+    
+  },
+  {
+    Header: 'Gender',
+    accessor: 'gender'
+  },
+  {
+    Header: 'Class',
+    accessor: 'class'
+  },
+  {
+    Header: 'Marks',
+    accessor: 'marks'
+  },
+]
